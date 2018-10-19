f83573da40dccf1211260a7cdf6ef955f152f2e6
Ralph Perdomo <ralph@prdmo.com>
2018-10-19 10:54:25 -0700

    tabulate user responses

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 $(function(){
+  $("#show-first").click(function(){
+    $("#q1").slideDown();
+  });
+
   $("#submit").click(function(){
+    var tally=0;
+    $(".question").each(function(i) {
 
-    $(".question").each(function() {
-      console.log($(this))
+      // checks all sections are completed on submit
+      var block = $(this).attr("id");
 
+      if ($("#"+block+" input:radio:checked").length) {
+        console.log("question "+parseInt(i+1)+" reponse = "+ $("#"+block+" input:radio:checked").val());
+        tally = tally + parseInt($("#"+block+" input:radio:checked").val())
+      } else {
+        console.log("student left question " +parseInt(i+1)+ " blank")
+        return false
+      }
+
+      console.log(tally)
+      debugger
 
     })
-    if ($("#q1 input:radio:checked").length) {
-      console.log($("#q1 input:radio:checked").val())
-    }
 
-    // console.log($("input[name=resp1]:radio:checked").val())
   })
 
 
