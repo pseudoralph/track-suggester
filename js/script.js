23d84334aededf289ff733271fec22b46a7b69a3
Ralph Perdomo <ralph@prdmo.com>
2018-10-19 12:41:04 -0700

    full logic added to prev/next button

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 $(function(){
 
   $("#start").click(function(){
-    $("#q1").addClass("is-active");
+    $(".quiz").slideDown();
   })
 
   $("#next").click(function(){
-
     var present = $(".is-active").prop("id")
-    next = present[0]+(parseInt(present[1])+1)
+    var next = present[0]+(parseInt(present[1])+1)
 
-    $("#"+present).removeClass("is-active")
-    $("#"+next).addClass("is-active")
+    if ($(".is-active input:radio:checked").length) {
+      console.log("selection made")
+      if (($(".question").length) > parseInt(present[1])){
+        $("#"+present).removeClass("is-active");
+        $("#"+next).addClass("is-active");
+      } else {console.log('end of questions')}
+
+    } 
+
+
+
   })
 
   $("#prev").click(function(){
-    var present = $(".is-active").prop("id")
-    prev = present[0]+(parseInt(present[1])-1)
 
-    $("#"+present).removeClass("is-active")
-    $("#"+prev).addClass("is-active")
+    var present = $(".is-active").prop("id")
+    var prev = present[0]+(parseInt(present[1])-1)
+
+    if (parseInt(present[1]-1)>0) {
+      $("#"+present).removeClass("is-active")
+      $("#"+prev).addClass("is-active")
+    } else {console.log('end of questions')}
+
+
+
 
   })
 
