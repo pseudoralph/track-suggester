e2642e1a5423f1c41455f374acd2c89a63a5c98a
Ralph Perdomo <ralph@prdmo.com>
2018-10-19 13:57:23 -0700

    quiz functions with appropriate response

--- js/script.js
+++ js/script.js
@@ -1,48 +1,54 @@
-$(function(){
+function whichTrack(score) {
+  var track = score/5;
 
+  if (track < 1.5) {return 'CSS/Design'}
+  else if (track < 2.4) {return 'Java/Android'}
+  return 'C#/.Net'
+}
+
+
+$(function(){
   $("#start").click(function(){
     $(".quiz").slideDown();
+    $("#q5 input:radio").change(function(){
+      $(".is-active #submit").show();
+    })
+    $("#start").hide();
   })
 
   $("#next").click(function(){
-    var present = $(".is-active").prop("id")
-    var next = present[0]+(parseInt(present[1])+1)
+    var present = $(".is-active").prop("id");
+    var next = present[0]+(parseInt(present[1])+1);
+
+    $(".is-active input:radio").change(function(){
+      $("p.make-selection").hide();
+    })
 
     if ($(".is-active input:radio:checked").length) {
-      console.log("selection made")
       if (($(".question").length) > parseInt(present[1])){
         $("#"+present).removeClass("is-active");
         $("#"+next).addClass("is-active");
-      } else {console.log('end of questions')}
-
-    } 
-
-
+      }
+    } else {
+      $("p.make-selection").show();
+    }
 
   })
 
   $("#prev").click(function(){
-
-    var present = $(".is-active").prop("id")
-    var prev = present[0]+(parseInt(present[1])-1)
+    var present = $(".is-active").prop("id");
+    var prev = present[0]+(parseInt(present[1])-1);
 
     if (parseInt(present[1]-1)>0) {
-      $("#"+present).removeClass("is-active")
-      $("#"+prev).addClass("is-active")
-    } else {console.log('end of questions')}
-
-
-
-
+      $("#"+present).removeClass("is-active");
+      $("#"+prev).addClass("is-active");
+    }
   })
-
-
 
 
   $("#submit").click(function(){
     var tally=0;
     $(".question").each(function(i) {
-
       // checks all sections are completed on submit
       var block = $(this).attr("id");
 
@@ -54,11 +60,14 @@ $(function(){
         return false
       }
 
-      console.log(tally)
-
     })
+
+    console.log(whichTrack(tally));
 
   })
 
 
 });
+
+
+// reset all selections $("input:radio:checked").prop("checked", false)
