7d40ac40eb6074cd217899004d865446241bb5ba
Ralph Perdomo <ralph@prdmo.com>
2018-10-19 10:21:41 -0700

    complete questions radio buttons with a11y

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(function(){
-  
-  console.log('ready.')
+  $("#submit").click(function(){
+
+    $(".question").each(function() {
+      console.log($(this))
+
+
+    })
+    if ($("#q1 input:radio:checked").length) {
+      console.log($("#q1 input:radio:checked").val())
+    }
+
+    // console.log($("input[name=resp1]:radio:checked").val())
+  })
 
 
 });
