2a4f4b567bb71d92c7bcb37dafc1886a373dc03b
Ralph Perdomo <ralph@prdmo.com>
2018-10-20 22:06:10 -0700

    complete prev/next logic

--- js/script.js
+++ js/script.js
@@ -1,42 +1,24 @@
 function canProceed(event) {
   var currentInt = parseInt($(".quiz").find(".is-active").prop("id")[1]);
+  $("#prev, #next").removeClass("proceed");
 
-  console.log("current id# q"+currentInt)
-
-
-  $("#prev, #next").removeClass("proceed")
-
-
-  if (event.type === 'change' && currentInt <5) {
+  if (event.type === 'change' && currentInt <=5) {
     if (currentInt > 1) {
       $(".is-active").parent().siblings("#prev").addClass("proceed");
-
-
     }
     $(".is-active").parent().siblings("#next").addClass("proceed");
     return;
   }
 
   if (event.type === 'click') {
-    // $(".is-active").parent().siblings("#proceed, #next").removeClass("proceed");
+    var canGoBack = ($(".is-active").parent().find("input:radio:checked").length);
     if (currentInt > 1) {
       $(".is-active").parent().siblings("#prev").addClass("proceed");
-
     }
-
+    if (canGoBack >= currentInt && $(".is-active #submit").css("display") !== 'inline-block') {
+      $(".is-active").parent().siblings("#next").addClass("proceed");
+    }
   }
-
-
-  if ($(".is-active span.make-selection").css("display") === "block") {
-    console.log("can't proceed")
-
-  } else {
-
-  }
-
-
-  // console.log($("input:radio"))
-
 }
 
 function whichTrack(score) {
@@ -45,7 +27,7 @@ function whichTrack(score) {
   if (track < 1.5) {
     return {
       title: 'CSS/Design',
-      summary: "You've got an eye for design and creativity! Persuing a track in CSS and design might be the right track for you. Often, this is the track persued by those who want to program on the web—specifically frontend UI.",
+      summary: "You've got an eye for design and creativity! Pursuing a track in CSS and design might be the right track for you. Often, this is the track pursued by those who want to program on the web—specifically frontend UI.",
       image: 'imgs/css.jpg'}
     }
   else if (track < 2.4) {
@@ -72,9 +54,9 @@ $(function(){
 
     // event listener created for 5th question to show submit button
     $("#q5 input:radio").change(function(){
-      $("#prev,#next").hide();
       $("#q5 .progress-bar").attr("style","width: 100%");
-      $("input:radio").prop("disabled", true)
+
+      $(".is-active").parent().siblings("#next").removeClass("proceed");
       $(".is-active #submit").show();
     })
 
@@ -102,50 +84,42 @@ $(function(){
   })
 
   $("#prev").click(function(e){
-
-
     var present = $(".is-active").prop("id");
     var prev = present[0]+(parseInt(present[1])-1);
-
     $("span.make-selection").hide();
-
     if (parseInt(present[1]-1)>0) {
       $("#"+present).removeClass("is-active");
       $("#"+prev).addClass("is-active");
     }
     canProceed(e);
-
-
   })
 
   $("#submit").click(function(){
     var tally=0;
-
     $(".question").each(function(i) {
       var block = $(this).attr("id");
       if ($("#"+block+" input:radio:checked").length) {
         tally = tally + parseInt($("#"+block+" input:radio:checked").val())
       }
-
     })
     $("#submit").hide();
+    $("#prev, #next").hide();
+    $("input:radio").prop("disabled", true)
 
     // render results card below
     $(".results #image").attr("src",whichTrack(tally).image);
     $(".results #title").text(whichTrack(tally).title);
     $(".results #summary").text(whichTrack(tally).summary);
+
     $(".results").show();
   })
 
   $("#restart").click(function(){
     $("input:radio:checked").prop("checked", false);
+    $("input:radio").prop("disabled", false);
+
     $(".is-active").removeClass("is-active");
-
-    $("input:radio").prop("disabled", false)
-
-
     $(".quiz, .results, #submit").hide();
     $("#prev, #next, #start").show();
-
   })
 });
